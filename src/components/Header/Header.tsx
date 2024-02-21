92c38b02bb4e548a066e4be1ca53a26d24a2368c
«ifUride» <ifuride@gmail.com>
2024-02-21 13:24:01 +0100

    Fix of Home page link in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <header className="header">
       <div className="header__content">
-        <a className="logo" href="/#">
+        <a className="logo" href="/">
           <img 
             className="logo__image"
             src="icons/fever_logo_2.svg" 

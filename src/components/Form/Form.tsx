e551afbb602ab5b96e5aca9f6b8d0969229edbf8
«ifUride» <ifuride@gmail.com>
2024-02-25 22:02:00 +0100

    Required field validation added

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useRef } from "react";
-import "./Form.scss";
-import { HourDropdown } from "../HourDropdown";
+import React, { FC, useRef, useState } from 'react';
+import classNames from 'classnames';
+import './Form.scss';
+import { HourDropdown } from '../HourDropdown';
 
 type Props = {
   onCalculate: (formState: Record<string, string>) => void,
@@ -10,6 +11,13 @@ type Props = {
 export const Form: FC<Props> = ({ 
   onCalculate, resultRef 
 }) => {
+  const [hasBatterySizeError, setHasBatterySizeError] = useState(false);
+  const [hasDailyConsumptionError, setHasDailyConsumptionError] = useState(false);
+  const [hasChargingSpeedError, setHasChargingSpeedError] = useState(false);
+  const [hasChargesAmountError, setHasChargesAmountError] = useState(false);
+  const [hasStartTimeError, setHasStartTimeError] = useState(false);
+  const [hasEndTimeError, setHasEndTimeError] = useState(false);
+
   const formRef = useRef(null);
   const formState: Record<string, string> = {};
   const executeScroll = () => {
@@ -17,15 +25,36 @@ export const Form: FC<Props> = ({
 
     resultRef.current.scrollIntoView({behavior: 'smooth'});
   };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (!formRef.current) return;
-
+  
     const formData = new FormData(formRef.current);
-
+    
     for (const [key, value] of formData.entries()) {
       formState[key] = value as string;
+    }
+
+    const {
+      batterySize,
+      dailyConsumption,
+      chargingSpeed,
+      amoutOfAssets,
+      startTime,
+      endTime,
+    } = formState;
+    
+    setHasBatterySizeError(!batterySize);
+    setHasDailyConsumptionError(!dailyConsumption);
+    setHasChargingSpeedError(!chargingSpeed);
+    setHasChargesAmountError(!amoutOfAssets);
+    setHasStartTimeError(!startTime);
+    setHasEndTimeError(!endTime);
+
+    if (!batterySize || !dailyConsumption || !chargingSpeed || !amoutOfAssets || !startTime || !endTime) {
+      return;
     }
 
     onCalculate(formState);
@@ -49,72 +78,130 @@ export const Form: FC<Props> = ({
           ref={formRef}
         >
           <div className="field">
-            <label className="label">Battery size(kWh)</label>
+            <label className="label" htmlFor="battery-size">Battery size (kWh)</label>
             <div className="control">
               <input
-                className="input"
+                className={classNames('input', {
+                  'is-danger': hasBatterySizeError,
+                })}
+                id="battery-size"
                 type="number"
                 name="batterySize"
                 placeholder="Battery size"
-                required
+                onChange={() => setHasBatterySizeError(false)}
               />
             </div>
+            {hasBatterySizeError && (
+              <p className="help is-danger">Battery size is required</p>
+            )}
           </div>
 
           <div className="field">
-            <label className="label">Daily consumption(percentage)</label>
+            <label 
+              className="label" 
+              htmlFor="daily-consumption"
+            >
+              Daily consumption (percentage)
+            </label>
             <div className="control">
               <input
-                className="input"
+                className={classNames('input', {
+                  'is-danger': hasDailyConsumptionError,
+                })}
+                id="daily-consumption"
                 type="number"
                 name="dailyConsumption"
                 min={0}
                 max={100}
                 step={1}
                 placeholder="Daily consumption"
+                onChange={() => setHasDailyConsumptionError(false)}
               />
             </div>
+            {hasDailyConsumptionError && (
+              <p className="help is-danger">Daily consumption is required</p>
+            )}
           </div>
 
           <div className="field">
-            <label className="label">Charging speed(kW)</label>
+            <label 
+              className="label"
+              htmlFor="charging-speed"
+            >
+              Charging speed (kW)
+            </label>
             <div className="control">
               <input
-                className="input"
+                id="charging-speed"
+                className={classNames('input', {
+                  'is-danger': hasChargingSpeedError,
+                })}
                 type="number"
                 name="chargingSpeed"
                 min={0}
                 placeholder="Charging speed"
+                onChange={() => setHasChargingSpeedError(false)}
               />
             </div>
+            {hasChargingSpeedError && (
+              <p className="help is-danger">Charging speed is required</p>
+            )}
           </div>
 
           <div className="field">
-            <label className="label">Amount of assets</label>
+            <label 
+              className="label"
+              htmlFor="amount-of-chargers"
+            >
+              Amount of chargers
+            </label>
             <div className="control">
               <input
-                className="input"
+                className={classNames('input', {
+                  'is-danger': hasChargesAmountError,
+                })}
+                id="amount-of-chargers"
                 type="number"
                 name="amoutOfAssets"
                 min={0}
                 step={1}
-                placeholder="Amount of assets"
+                placeholder="Amount of chargers"
+                onChange={() => setHasChargesAmountError(false)}
               />
             </div>
+            {hasChargesAmountError && (
+              <p className="help is-danger">Amount of chargers is required</p>
+            )}
           </div>
 
           <div className="field">
             <label className="label">Availability start</label>
-            <div className="select">
-              <HourDropdown name="startTime" />
+            <div className={classNames('select', {
+            'is-danger': hasStartTimeError,
+          })}>
+              <HourDropdown 
+                name="startTime" 
+                setHasError={setHasStartTimeError}
+              />
             </div>
+            {hasStartTimeError && (
+              <p className="help is-danger">Availability start is required</p>
+            )}
           </div>
 
           <div className="field">
             <label className="label">Availability end</label>
-            <div className="select">
-              <HourDropdown name="endTime" />
+            <div className={classNames('select', {
+            'is-danger': hasEndTimeError,
+          })}>
+              <HourDropdown 
+                name="endTime" 
+                setHasError={setHasEndTimeError}
+              />
             </div>
+            {hasEndTimeError && (
+              <p className="help is-danger">Availability end is required</p>
+            )}
           </div>
 
           <div className="field">
--- src/components/HourDropdown/HourDropdown.tsx
+++ src/components/HourDropdown/HourDropdown.tsx
@@ -1,17 +1,23 @@
-import { FC } from "react";
+import { FC } from 'react';
 
 type Props = {
   name: string,
+  setHasError: (flag: boolean) => void,
 };
 
-export const HourDropdown: FC<Props>  = ({ name }) => {
+export const HourDropdown: FC<Props>  = ({ name, setHasError }) => {
   const hours = Array.from({ length: 24 }, (_, index) => index);
 
   return (
-    <select name={name}>
+    <select name={name} onChange={() => setHasError(false)}>
       <option value="">Select time</option>
       {hours.map(hour => (
-        <option key={hour} value={hour}>{hour.toString().padStart(2, '0')}:00</option>
+        <option 
+          key={hour}
+          value={hour}
+        >
+            {hour.toString().padStart(2, '0')}:00
+        </option>
       ))}
     </select>
   );
